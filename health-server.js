8049947934c897c04ed2f13073d9029b25590df4
himanshubhardwajprovar <himanshubhardwajprovar@gmail.com>
2025-09-10 15:33:28 +0530

    added health server file

---
+++ health-server.js
@@ -0,0 +1,18 @@
+// Simple health check server for Docker/ALB
+const http = require('http');
+
+const PORT = 8931;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/health' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('OK');
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Health check server running on port ${PORT}`);
+});
